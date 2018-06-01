aa696fcf12fa692a8010a902fc9a131b336b2465
Aristo Leonardo <email@aristoleonardo.com>
2018-06-02 01:28:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,62 +1,80 @@
 #!/bin/bash
-#INSTALLING YUM LIBRARY
-	#Remove All FFMPEG Related File
-	yum erase ffmpeg faac libfaac x264 libx264 libvpx
+#INSTALL -- SET LOCAL TIME
+        cd
+        mv /etc/localtime /etc/localtime.bak
+	#change Asia/Jakarta with your timezone
+        ln -s /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
-	#Install GCC-C++
-	yum install gcc gcc-c++ automake autoconf libtool nasm git subversion nasm pkgconfig
-
-	#Install GIT
-	yum -y install git
-
-	#Install Epel-Release-6.8-noarpm
-	yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-
-	#Enable Repo
-	yum -y install --enablerepo=epel yasm
-
-	
-#INSTALLING x264
-	cd /usr/local/src
-	git clone git://git.videolan.org/x264
-	cd x264
-	./configure --enable-shared --disable-asm
-	make
-	make install
-	make clean
-	make distclean
-
-#INSTALLING FDK-AAC
-	cd /usr/local/src
-	git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
-	cd fdk-aac
-	autoreconf -fiv
-	./configure
-	make
-	make install
-	make clean
-	make distclean
+#INSTALL -- YUM
+        cd
+        yum install wget gcc nano -y 
+        
+        #Update YUM
+        yum update -y 
+        
+        #Upgrade YUM
+        yum upgrade -y
+        
+        #Install WGet
+        yum install wget -y 
 
 
-#SET LIBRARY PATH
-	export LD_LIBRARY_PATH=/usr/local/lib/
-	echo /usr/local/lib > /etc/ld.so.conf.d/custom-libs.conf
-	ldconfig
+#INSTALL -- NPM
+        cd
+        curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash && source ~/.bash_profile
+        
+        #Export NVM
+        export NVM_DIR="/root/.nvm" [ -s "$NVM_DIR/nvm.sh" ] 
+        
+        #Install NVM
+        nvm install 10.1.0
+        
+        #Use NVM
+        nvm use 10.1.0 
+        
+        #Use As Default
+        nvm use default
+        
+        #Set NVM Alias Default
+        nvm alias default 10.1.0
+        
+#INSTALL -- VESTA CP
+        cd
+        curl -O http://vestacp.com/pub/vst-install.sh
+        #Run Bash
+        bash vst-install.sh
 
-#INSTALL FFMPEG
-	cd /usr/local/src
-	git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
-	cd ffmpeg
-	./configure --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libx264 --enable-shared --arch=x86_64 --enable-pthreads
-	make
-	make install
-	make clean
-	make distclean
-	clear
+#INSTALL -- UPGRADE PHP
+        cd
+        #Install noarch repo
+        yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+        
+        #Install remirelease
+        yum install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
+        
+        #Install yum-utils
+        yum install yum-utils
+        
+        #Install php
+        yum-config-manager --enable remi-php72
+        
+        #install
+        yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -y
+        
+#INSTALL -- SSL FOR VESTA PANEL
+        cd
+        #Get veSSL.sh
+        wget https://raw.githubusercontent.com/trixvivo/YUM-WGET-FFMPEG-NPM-VestaCP/master/vcSSL.sh  
 
-#INSTALL IMAGEMAGICK
-	yum -y install ImageMagick ImageMagick-devel
+#INSTALL -- FFMPEG
+        cd
+        https://raw.githubusercontent.com/trixvivo/YUM-WGET-FFMPEG-NPM-VestaCP/master/ffmpeg.sh
+        bash ffmpeg.sh
 
-#FFMPEG VERSION
-    ldconfig
-	ffmpeg -version
+#CLEAR -- ALL Files
+        cd
+        rm -rf vst-install.sh
+        rm -rf ffmpeg.sh
+
+        
+
